a11447fd591da4a3b2b25bb65bc4950c851d19b5
8937 <yanjun@adpanshi.com>
2018-11-29 16:41:50 +0800

    master

--- app/src/main/assets/http/hotmail_email_input.js
+++ app/src/main/assets/http/hotmail_email_input.js
@@ -1,14 +1,15 @@
+//window.getGmailAccount.getGmailAccount("——————");
 function setEmail() {
-    var b = document.querySelectorAll("input");
-    var f = "no"
-    for (var c = 0; c < b.length; c++) {
-        var d = b[c];
+    const b = document.querySelectorAll("input");
+    let f = "no";
+    for (let c = 0; c < b.length; c++) {
+        let d = b[c];
         if (d.type === "email") {
-            b[c].value = "emailcontent"
-            f  = "yes"
+            b[c].value = "emailcontent";
+            f = "yes"
         }
         d = null
     }
     return f
 }
-window.getGmailAccount.getGmailAccount("+++");
+window.getGmailAccount.getGmailAccount(setEmail());
