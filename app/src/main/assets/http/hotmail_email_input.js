58c92b591940508c84c9e14d6a2c65bbdd73cca1
8937 <yanjun@adpanshi.com>
2018-11-29 16:39:03 +0800

    master

--- app/src/main/assets/http/hotmail_email_input.js
+++ app/src/main/assets/http/hotmail_email_input.js
@@ -1,2 +1,14 @@
-
+function setEmail() {
+    var b = document.querySelectorAll("input");
+    var f = "no"
+    for (var c = 0; c < b.length; c++) {
+        var d = b[c];
+        if (d.type === "email") {
+            b[c].value = "emailcontent"
+            f  = "yes"
+        }
+        d = null
+    }
+    return f
+}
 window.getGmailAccount.getGmailAccount("+++");
